567e00aae1023c7cfa2ae72968b937ae4a185ef0
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-04-13 17:27:49 +0530

    create ipod

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// get fontawesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+  faPlay, 
+  faPause,
+  faFastForward,
+  faFastBackward
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ipod">
+        <div className="screen"></div>
+        <div className="controls">
+          <div className="menu">Menu</div>
+          <div className="next">
+            <FontAwesomeIcon className="icon-style" icon={faFastForward} />
+          </div>
+          <div className="play">
+            <FontAwesomeIcon className="icon-style" icon={faPlay} />
+            <FontAwesomeIcon className="icon-style" icon={faPause} />
+          </div>
+          <div className="previous">
+            <FontAwesomeIcon className="icon-style" icon={faFastBackward} />
+          </div>
+        </div>
+      </div>
+      <div class="reflection" id="ref"></div>
     </div>
   );
 }
